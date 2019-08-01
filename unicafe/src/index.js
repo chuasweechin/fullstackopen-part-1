d88afea9bef77a3ff9dbccbed68b0ec3460f9ac2
chuasweechin <sampiochua@gmail.com>
2019-08-01 14:03:25 +0800

    complete part 1.2 unicafe exercise

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,80 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-    return(
-        <h1>
-            { props.name }
-        </h1>
+
+const FeedbackButton = ({ text, clickHandler }) => {
+    return (
+        <button onClick={ () => { clickHandler(text) } }>
+            { text }
+        </button>
     )
 }
 
-const Content = (props) => {
-    return(
-        <ul>
-            <Part
-                part = { props.parts[0] }
-            />
-            <Part
-                part = { props.parts[1] }
-            />
-            <Part
-                part = { props.parts[2] }
-            />
-        </ul>
+const Statistics = ({ good, neutral, bad }) => {
+    if ( good <= 0 && neutral <= 0 && bad <= 0) {
+        return (
+            <div>No feedback given</div>
+        )
+
+    } else {
+        const all = good + neutral + bad;
+
+        return (
+            <table>
+                <tbody>
+                    <Statistic
+                        text="good"
+                        value={ good }
+                    />
+                    <Statistic
+                        text="neutral"
+                        value={ neutral }
+                    />
+                    <Statistic
+                        text="bad"
+                        value={ bad }
+                    />
+                    <Statistic
+                        text="all"
+                        value={ all }
+                    />
+                    <Statistic
+                        text="average"
+                        value={ (good - bad) / all + "%" }
+                    />
+                    <Statistic
+                        text="positive"
+                        value={ good / all * 100 + "%" }
+                    />
+                </tbody>
+            </table>
+        )
+    }
+}
+
+const Statistic = ({ text, value }) => {
+    return (
+        <tr>
+            <td>
+                { text }: { value }
+            </td>
+        </tr>
     )
 }
 
-const Part = (props) => {
-    return(
-        <li>
-            { props.part.name } { props.exercises }
-        </li>
-    )
-}
-
-const Total = (props) => {
-    return(
-        <p>
-            Number of exercises: { props.parts[0].exercises
-                                    + props.parts[1].exercises
-                                    + props.parts[2].exercises }
-        </p>
-    )
-}
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleClick = (t) => {
+        if (t === "good") { setGood(good + 1) }
+        if (t === "neutral") { setNeutral(neutral + 1) }
+        if (t === "bad") { setBad(bad + 1) }
     }
 
     return (
-    <div>
-        <Header
-            name = { course.name }
-        />
-        <Content
-            parts = { course.parts }
-        />
-        <Total
-            parts = { course.parts }
-        />
-    </div>
+        <div>
+            <h1>Give Feedback</h1>
+            <FeedbackButton
+                text= "good"
+                clickHandler= { handleClick }
+            />
+            <FeedbackButton
+                text= "neutral"
+                clickHandler= { handleClick }
+            />
+            <FeedbackButton
+                text= "bad"
+                clickHandler= { handleClick }
+            />
+
+            <h1>Statistics</h1>
+            <Statistics
+                good={ good }
+                neutral={ neutral }
+                bad={ bad }
+            />
+        </div>
     )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />,
+    document.getElementById('root')
+)
